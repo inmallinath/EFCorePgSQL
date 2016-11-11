8bb295a6823598c8875f0c3c1090624c53b3c90d
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-11-11 14:03:07 -0800

    Add logic to add and display data on the main program

--- Program.cs
+++ Program.cs
@@ -8,5 +8,27 @@ namespace ConsoleApplication
         {
             Console.WriteLine("Hello World!");
         }
+		
+		private static void addStudents()
+		{
+			using (var db = new InstituteContext()) {
+				db.Students.AddRange(Student.GetSampleStudents());
+				var count = db.SaveChanges();
+				Console.WriteLine("{0} records saved to database", count);
+			}
+		}
+		
+		private static void displayStudents() 
+		{
+			using (var db = new InstituteContext()) 
+			{
+				Console.WriteLine();
+				Console.WriteLine("All students in database:");
+				foreach (var s in db.Students) 
+				{
+					Console.WriteLine("{0}\t{1}\t{2}", s.Id, s.FirstName, s.LastName);
+				}
+			}
+		}
     }
 }
